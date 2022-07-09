89c53449cf8253f15b0d437b0e9b0857a5e257ca
Anton Kuzmenko <1917237+NoNonsenseDev@users.noreply.github.com>
2022-07-08 23:02:36 -0700

    [feat] Generate movies.json

---
+++ generate_large_file.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import orjson
+import random
+import string
+
+
+def generate_large_file(file_name, number_of_movies):
+
+    movies = []
+    for _ in range(number_of_movies):
+        movie = {
+            "title": "".join(random.choice(string.ascii_lowercase) for _ in range(10)),
+            "year": random.randint(1900, 2020),
+            "director": "".join(
+                random.choice(string.ascii_lowercase) for _ in range(10)
+            ),
+            "rating": random.randint(1, 5),
+        }
+        movies.append(movie)
+
+    with open(file_name, "wb") as f:
+        f.write(orjson.dumps(movies))
+
+
+def main():
+    # expose number of movies to generate in the command line
+    number_of_movies = int(input("Enter number of movies: "))
+    generate_large_file("data/movies.json", number_of_movies)
+
+
+if __name__ == "__main__":
+    main()
