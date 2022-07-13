eb92e4b96635c0857ab8ec11c1887efad05eb423
Anton Kuzmenko <1917237+NoNonsenseDev@users.noreply.github.com>
2022-07-12 23:45:40 -0700

    [feat] Add helper to measure perf

---
+++ utils.py
@@ -0,0 +1,11 @@
+from collections.abc import Generator
+from contextlib import contextmanager
+from time import perf_counter
+
+
+@contextmanager
+def measure(context: str) -> Generator[None, None, None]:
+    perf_start = perf_counter()
+    yield
+    perf_end = perf_counter()
+    print(f"{context}: {perf_end - perf_start:.2f}\n")
